cdb1b65c5334f2b92982e7d03bcaa63cf5029c0f
Joost Prins <joost_prins@hotmail.com>
2016-03-10 09:36:53 +0100

    linkcost and neighbours set in map

--- src/challenge5/client/BasicRoute.java
+++ src/challenge5/client/BasicRoute.java
@@ -6,5 +6,32 @@ package challenge5.client;
  * @version 09-03-2015
  */
 public class BasicRoute extends AbstractRoute {
-
+	
+	private int cost;
+	
+	public BasicRoute(int destination, int nextHop, int linkCost) {
+		super.destination = destination;
+		super.nextHop = nextHop;
+		this.cost = linkCost;
+	}
+	
+	public int getDestination() {
+		return super.destination;
+	}
+	
+	public int getNextHop() {
+		return super.nextHop;
+	}
+	
+	public int getLinkCost() {
+		return this.cost;
+	}
+	
+	public void setNextHop(int nextHop) {
+		super.nextHop = nextHop;
+	}
+	
+	public void setLinkCost(int linkCost) {
+		this.cost = linkCost;
+	}
 }
