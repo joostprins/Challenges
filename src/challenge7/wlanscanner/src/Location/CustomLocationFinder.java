a1b5e2c895cca6c2ff6513a9b35f08c6fe895fa5
Tom Leemreize <leemreizetom@gmail.com>
2016-03-31 14:05:36 +0200

    Add new implementation

--- src/challenge7/wlanscanner/src/Location/CustomLocationFinder.java
+++ src/challenge7/wlanscanner/src/Location/CustomLocationFinder.java
@@ -29,6 +29,8 @@ public class CustomLocationFinder implements LocationFinder {
             }
         }
 
+        System.out.println(accessPoints.toString());
+
         if (accessPoints.size() == 3) {
             double x1 = knownLocations.get(accessPoints.get(0).getMacAsString()).getX();
             double y1 = knownLocations.get(accessPoints.get(0).getMacAsString()).getY();
@@ -38,58 +40,18 @@ public class CustomLocationFinder implements LocationFinder {
             double y2 = knownLocations.get(accessPoints.get(1).getMacAsString()).getY();
             double r2 = getDistance(accessPoints.get(1).getRssi());
             System.out.println("X2: " + x2 + " - Y2: " + y2 + " - R2: " + r2 + " (RSSI: " + accessPoints.get(1).getRssi() + ")");
-            double x3 = knownLocations.get(accessPoints.get(2).getMacAsString()).getX();
-            double y3 = knownLocations.get(accessPoints.get(2).getMacAsString()).getY();
-            double r3 = getDistance(accessPoints.get(2).getRssi());
-            System.out.println("X3: " + x3 + " - Y3: " + y3 + " - R3: " + r3 + " (RSSI: " + accessPoints.get(2).getRssi() + ")");
-            calculateThreeCircleIntersection(x1, y1, r1, x2, y2, r2, x3, y3, r3);
+
+            if (accessPoints.get(2).getRssi() > -50) {
+                return calculateIntersection(x1, y1, r1, x2, y2, r2)[0];
+            } else {
+                return calculateIntersection(x1, y1, r1, x2, y2, r2)[1];
+            }
+
         } else {
             System.out.println("Less / more than 3 APs.");
         }
 
-        //printMacs(data); //print all the received data
         return new Position(x,y); //return the first known APs location
-    }
-
-    /**
-     * Returns the position of the first known AP found in the list of MacRssi pairs
-     * @param data
-     * @return
-     */
-    private Position getFirstKnownFromList(MacRssiPair[] data){
-        Position ret = new Position(0,0);
-        for(int i=0; i<data.length; i++){
-            if(knownLocations.containsKey(data[i].getMacAsString())){
-                getDistance(data[i].getRssi());
-                ret = knownLocations.get(data[i].getMacAsString());
-            }
-        }
-        return ret;
-    }
-
-    /**
-     * Outputs all the received MAC RSSI pairs to the standard out
-     * This method is provided so you can see the data you are getting
-     * @param data
-     */
-    private void printMacs(MacRssiPair[] data) {
-        for (MacRssiPair pair : data) {
-            System.out.print(pair.getRssi() + " - ");
-            System.out.println(getDistance(pair.getRssi()));
-        }
-    }
-
-    public int getAverageRssi(MacRssiPair[] data) {
-        int total = 0;
-        int count = 0;
-        for (MacRssiPair pair : data) {
-            total += pair.getRssi();
-            count++;
-        }
-        if (count != 0) {
-            return total / count;
-        }
-        return 0;
     }
 
     public double getDistance(int rssi) {
@@ -97,7 +59,7 @@ public class CustomLocationFinder implements LocationFinder {
         return Math.pow(2.71828182845905d, power);
     }
 
-    private boolean calculateThreeCircleIntersection(double x0, double y0, double r0, double x1, double y1, double r1, double x2, double y2, double r2) {
+    private Position[] calculateIntersection(double x0, double y0, double r0, double x1, double y1, double r1) {
         double a, dx, dy, d, h, rx, ry;
         double point2_x, point2_y;
 
@@ -108,19 +70,7 @@ public class CustomLocationFinder implements LocationFinder {
         dy = y1 - y0;
 
     /* Determine the straight-line distance between the centers. */
-        d = Math.sqrt((dy*dy) + (dx*dx));
-
-    /* Check for solvability. */
-        if (d > (r0 + r1))
-        {
-        /* no solution. circles do not intersect. */
-            return false;
-        }
-        if (d < Math.abs(r0 - r1))
-        {
-        /* no solution. one circle is contained in the other */
-            return false;
-        }
+        d = Math.sqrt((dy * dy) + (dx * dx));
 
     /* 'point 2' is the point where the line through the circle
     * intersection points crosses the line between the circle
@@ -128,22 +78,22 @@ public class CustomLocationFinder implements LocationFinder {
     */
 
     /* Determine the distance from point 0 to point 2. */
-        a = ((r0*r0) - (r1*r1) + (d*d)) / (2.0 * d) ;
+        a = ((r0 * r0) - (r1 * r1) + (d * d)) / (2.0 * d);
 
     /* Determine the coordinates of point 2. */
-        point2_x = x0 + (dx * a/d);
-        point2_y = y0 + (dy * a/d);
+        point2_x = x0 + (dx * a / d);
+        point2_y = y0 + (dy * a / d);
 
     /* Determine the distance from point 2 to either of the
     * intersection points.
     */
-        h = Math.sqrt((r0*r0) - (a*a));
+        h = Math.sqrt((r0 * r0) - (a * a));
 
     /* Now determine the offsets of the intersection points from
     * point 2.
     */
-        rx = -dy * (h/d);
-        ry = dx * (h/d);
+        rx = -dy * (h / d);
+        ry = dx * (h / d);
 
     /* Determine the absolute intersection points. */
         double intersectionPoint1_x = point2_x + rx;
@@ -153,29 +103,11 @@ public class CustomLocationFinder implements LocationFinder {
 
         System.out.println("INTERSECTION Circle1 AND Circle2: (" + intersectionPoint1_x + "," + intersectionPoint1_y + ")" + " AND (" + intersectionPoint2_x + "," + intersectionPoint2_y + ")");
 
-    /* Lets determine if circle 3 intersects at either of the above intersection points. */
-        dx = intersectionPoint1_x - x2;
-        dy = intersectionPoint1_y - y2;
-        double d1 = Math.sqrt((dy*dy) + (dx*dx));
-
-        dx = intersectionPoint2_x - x2;
-        dy = intersectionPoint2_y - y2;
-        double d2 = Math.sqrt((dy*dy) + (dx*dx));
-
-        if(Math.abs(d1 - r2) < EPSILON) {
-            System.out.println("INTERSECTION1 Circle1 AND Circle2 AND Circle3: (" + intersectionPoint1_x + "," + intersectionPoint1_y + ")");
-            this.x = intersectionPoint1_x;
-            this.y = intersectionPoint1_y;
+        if (intersectionPoint1_y > intersectionPoint2_y) {
+            return new Position[]{new Position(intersectionPoint1_x, intersectionPoint1_y), new Position(intersectionPoint2_x, intersectionPoint2_y)};
+        } else {
+            return new Position[]{new Position(intersectionPoint2_x, intersectionPoint2_y), new Position(intersectionPoint1_x, intersectionPoint1_y)};
         }
-        else if(Math.abs(d2 - r2) < EPSILON) {
-            System.out.println("INTERSECTION2 Circle1 AND Circle2 AND Circle3: (" + intersectionPoint2_x + "," + intersectionPoint2_y + ")"); //here was an error
-            this.x = intersectionPoint2_x;
-            this.y = intersectionPoint2_y;
-        }
-        else {
-            System.out.println("INTERSECTION Circle1 AND Circle2 AND Circle3: NONE");
-        }
-        return true;
     }
 
 }
